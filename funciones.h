e0f73aac59ac868303fc27cf2869e68f8b2c03e0
cruzsebas1239-create <cruzsebas1239@gmail.com>
2026-05-04 19:26:52 -0500

    Add header file for mathematical functions

---
+++ funciones.h
@@ -0,0 +1,9 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+double factorial(int n);
+double senoTaylor(double x, int n);
+double cosenoTaylor(double x, int n);
+double exponencialTaylor(double x, int n);
+
+#endif
